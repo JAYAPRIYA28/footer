5ad38136a206b1f496882bf5dfc9f78aa9a3a65f
JAYAPRIYA28 <83283403+JAYAPRIYA28@users.noreply.github.com>
2021-11-26 21:50:13 +0530

    Add files via upload

---
+++ src/Component/Footer.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+
+
+
+
+
+
+function Footer(){
+    return(
+        
+        <div className="bg-white mt-20 shadow-2xl ml-2 mr-2">
+          
+        
+        
+          <div className="grid lg:grid-cols-5 items-center sm:grid-cols-4  lg:gap-10 sm:gap-5 pt-20 pl-8  ">
+
+
+          <div className="sm:pl-5 lg:pl-0 lg:col-span-1 sm:col-span-4 sm:pt-5 lg:pt-0 ">
+          <img className="lg:w-44 sm:w-96 " src="assets/MicrosoftTeams-image.1.png"></img>
+          <p className="lg:pt-5 sm:pt-7 lg:text-sm  sm:text-2xl  lg:pb-5">Lorem Ipsum is simply dummy text of the printing and typesettiong industry</p>
+          <p className="flex ">
+        
+          <img className="lg:w-5 lg:block sm:hidden  sm:w-8 lg:h-8 sm:h-14 lg:mt-3 sm:mt-6 lg:ml-4 sm:ml-64"   src="assets/icons8-twitter-48.png"></img>
+          <img className="lg:w-7 lg:block sm:hidden sm:w-10 lg:h-8 sm:h-14 lg:mt-3 sm:mt-6 lg:ml-4 sm:ml-8" src="assets/icons8-linkedin-48.png"></img> 
+           <img className="lg:w-5 lg:block sm:hidden sm:w-8 lg:h-6 sm:h-11 lg:mt-4 sm:mt-6 lg:ml-4 sm:ml-8" src="assets/facebook.png"></img>
+           <img className="lg:w-5 lg:block sm:hidden sm:w-8 lg:h-6 sm:h-11 lg:mt-4 sm:mt-6 lg:ml-4 sm:ml-8" src="assets/icons8-google-48.png"></img>
+          </p>
+          <p className="lg:mt-5 lg:block sm:hidden sm:mt-7 lg:pl-0 sm:pl-56 text-purple-900 font-sans lg:text-sm sm:text-xl lg:mb-3">@2021 insightmonk.com</p>
+          </div>
+          
+          <div className=" sm:pl-5   lg:pt-0 ">
+        
+          <p>
+              <p className=" text-purple-900 lg:text-lg sm:text-2xl lg:pt-5 ">Terms of Use</p>
+              <p className=" text-purple-900 lg:text-lg sm:text-2xl lg:pt-5 sm:pt-10 ">Privacy Policy</p>
+              <p className=" text-purple-900 lg:text-lg sm:text-2xl lg:pt-5 sm:pt-10 lg:pb-0 sm:pb-28">Contact Us</p>
+              
+          </p>
+          </div>
+          <div className="sm:pl-5 lg:pl-0 sm:pt-5 lg:pt-0 ">
+          
+          <p>
+              <p className=" text-purple-900 lg:text-lg sm:text-2xl lg:pt-5 sm:pt-10">Question & Answer</p>
+              <p className=" text-purple-900 lg:text-lg sm:text-2xl lg:pt-5 sm:pt-10">Reports</p>
+              <p className=" text-purple-900 lg:text-lg sm:text-2xl lg:pt-5 sm:pt-10">Public sources</p>
+              <p className=" text-purple-900 lg:text-lg sm:text-2xl lg:pt-5 sm:pt-10">Market statistic</p>
+              <p className=" text-purple-900 lg:text-lg sm:text-2xl lg:pt-5 sm:pt-10">Polls</p>
+          </p>
+          </div>
+          <div className="sm:pl-5 lg:pl-0 sm:pt-5 lg:pt-0 ">
+          
+
+          <p>
+              <p className=" text-purple-900 lg:text-lg sm:text-2xl lg:pt-5 sm:pt-10">Precision Medicine</p>
+              <p className="text-purple-900 lg:text-lg sm:text-2xl lg:pt-5 sm:pt-10">Healthcare Robotics</p>
+              <p className="text-purple-900 lg:text-lg sm:text-2xl lg:pt-5 sm:pt-10">Crypto</p>
+              <p className="text-purple-900 lg:text-lg sm:text-2xl lg:pt-5 sm:pt-10 lg:pb-0 sm:pb-9">Space Technology</p>
+          </p>
+          </div>
+          <div className="sm:pl-5 lg:pl-0  ">
+      
+          <p>
+              <p className=" text-purple-900 lg:text-lg sm:text-2xl lg:pt-5 ">Contact Us</p>
+              <p className="text-purple-900 lg:text-lg sm:text-2xl lg:pt-5 sm:pt-10 lg:pb-0 sm:pb-44">Help</p>
+          </p>
+          </div>
+          </div>
+         <div className="lg:hidden sm:block text-center pt-10 "> 
+         <p className="lg:mt-5   sm:mt-7  text-purple-900 font-sans  sm:text-2xl  pr-10">@2021 insightmonk.com</p>
+         
+        <p className="flex pl-20 pt-10">
+        
+        
+        <img className="  sm:w-8  sm:h-14  sm:mt-6  sm:ml-64"   src="assets/icons8-twitter-48.png"></img>
+        <img className=" sm:w-10  sm:h-14  sm:mt-6  sm:ml-8" src="assets/icons8-linkedin-48.png"></img> 
+         <img className=" sm:w-8  sm:h-11  sm:mt-6  sm:ml-8" src="assets/facebook.png"></img>
+         <img className=" sm:w-8  sm:h-11  sm:mt-6  sm:ml-8" src="assets/icons8-google-48.png"></img>
+        </p>
+        </div>
+        </div>
+     
+    )
+}
+
+export default Footer;
