320d9e6e46f6eddec4764e21c5c486d7143c3c6e
JAYAPRIYA28 <83283403+JAYAPRIYA28@users.noreply.github.com>
2021-11-27 21:35:30 +0530

    Update Footer.jsx

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -16,14 +16,16 @@ function Footer(){
 
 
           <div className="sm:pl-5 lg:pl-0 lg:col-span-1 sm:col-span-4 sm:pt-5 lg:pt-0 ">
-          <img className="lg:w-44 sm:w-96 " src="assets/MicrosoftTeams-image.1.png"></img>
+          
+          <img className="lg:w-44 sm:w-96 " alt="logo" src="assets/MicrosoftTeams-image.1.png"></img> 
+          
           <p className="lg:pt-5 sm:pt-7 lg:text-sm  sm:text-2xl  lg:pb-5">Lorem Ipsum is simply dummy text of the printing and typesettiong industry</p>
           <p className="flex ">
         
-          <img className="lg:w-5 lg:block sm:hidden  sm:w-8 lg:h-8 sm:h-14 lg:mt-3 sm:mt-6 lg:ml-4 sm:ml-64"   src="assets/icons8-twitter-48.png"></img>
-          <img className="lg:w-7 lg:block sm:hidden sm:w-10 lg:h-8 sm:h-14 lg:mt-3 sm:mt-6 lg:ml-4 sm:ml-8" src="assets/icons8-linkedin-48.png"></img> 
-           <img className="lg:w-5 lg:block sm:hidden sm:w-8 lg:h-6 sm:h-11 lg:mt-4 sm:mt-6 lg:ml-4 sm:ml-8" src="assets/facebook.png"></img>
-           <img className="lg:w-5 lg:block sm:hidden sm:w-8 lg:h-6 sm:h-11 lg:mt-4 sm:mt-6 lg:ml-4 sm:ml-8" src="assets/icons8-google-48.png"></img>
+          <img className="lg:w-5 lg:block sm:hidden  sm:w-8 lg:h-8 sm:h-14 lg:mt-3 sm:mt-6 lg:ml-4 sm:ml-64" alt="twitter_logo"  src="assets/icons8-twitter-48.png"></img>
+          <img className="lg:w-7 lg:block sm:hidden sm:w-10 lg:h-8 sm:h-14 lg:mt-3 sm:mt-6 lg:ml-4 sm:ml-8" alt="linkedIn_logo" src="assets/icons8-linkedin-48.png"></img> 
+           <img className="lg:w-5 lg:block sm:hidden sm:w-8 lg:h-6 sm:h-11 lg:mt-4 sm:mt-6 lg:ml-4 sm:ml-8" alt="facebook_logo" src="assets/facebook.png"></img>
+           <img className="lg:w-5 lg:block sm:hidden sm:w-8 lg:h-6 sm:h-11 lg:mt-4 sm:mt-6 lg:ml-4 sm:ml-8" alt="google_logo" src="assets/icons8-google-48.png"></img>
           </p>
           <p className="lg:mt-5 lg:block sm:hidden sm:mt-7 lg:pl-0 sm:pl-56 text-purple-900 font-sans lg:text-sm sm:text-xl lg:mb-3">@2021 insightmonk.com</p>
           </div>
@@ -71,10 +73,10 @@ function Footer(){
         <p className="flex pl-20 pt-10">
         
         
-         <img className="  sm:w-10  sm:h-14  sm:mt-6  sm:ml-64"   src="assets/icons8-twitter-48.png"></img>
-         <img className=" sm:w-12  sm:h-16  sm:mt-4 sm:mb-3   sm:ml-8" src="assets/icons8-linkedin-48.png"></img> 
-         <img className=" sm:w-10  sm:h-11  sm:mt-6  sm:ml-8" src="assets/facebook.png"></img>
-         <img className=" sm:w-10  sm:h-11  sm:mt-6  sm:ml-8" src="assets/icons8-google-48.png"></img>
+        <img className="  sm:w-10  sm:h-14  sm:mt-6  sm:ml-64" alt="twitter_logo"  src="assets/icons8-twitter-48.png"></img>
+        <img className=" sm:w-12  sm:h-16  sm:mt-4 sm:mb-3   sm:ml-8" alt="linkedin_logo"  src="assets/icons8-linkedin-48.png"></img> 
+         <img className=" sm:w-10  sm:h-11  sm:mt-6  sm:ml-8" alt="facebook_logo"  src="assets/facebook.png"></img>
+         <img className=" sm:w-10  sm:h-11  sm:mt-6  sm:ml-8" alt="google_logo"  src="assets/icons8-google-48.png"></img>
         </p>
         </div>
         </div>
